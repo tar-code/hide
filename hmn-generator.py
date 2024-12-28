b60126dc801e6b2dc24b9c8187a8b18d8a6d47df
TarCode <viktorprishchepa@yandex.ru>
2024-12-28 15:20:21 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,47 +1,29 @@
-import time
 import requests
-from IPython.display import display, HTML
 
 url = 'https://hdmn.cloud/ru/demo/'
 
-def send_email():
-    try:
-        response = requests.get(url)
+# Попытка получить страницу и проверить статус ответа
+try:
+    response = requests.get(url)
+    
+    # Проверка на успешный ответ от сервера
+    if response.status_code == 200:
+        # Если текст на странице содержит "Ваша электронная почта", продолжаем
+        if 'Ваша электронная почта' in response.text:
+            email = input('Введите электронную почту для получения тестового периода: ')
 
-        # Проверка на успешный ответ от сервера
-        if response.status_code == 200:
-            # Если текст на странице содержит "Ваша электронная почта", продолжаем
-            if 'Ваша электронная почта' in response.text:
-                email = input('Введите электронную почту для получения тестового периода: ')
+            response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
+                "demo_mail": f"{email}"
+            })
 
-                response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
-                    "demo_mail": email
-                })
-
-                if 'Ваш код выслан на почту' in response.text:
-                    print('Подтвердите e-mail. Проверьте свой почтовый ящик.')
-                    # Отображение ссылки для повторного запроса
-                    display_retry_link()
-                else:
-                    print('Указанная почта не подходит для получения тестового периода.')
+            if 'Ваш код выслан на почту' in response.text:
+                print('Подтвердите e-mail. Проверьте свой почтовый ящик.')
             else:
-                print('На странице не найдено нужного текста. Проверьте доступность страницы.')
+                print('Указанная почта не подходит для получения тестового периода.')
         else:
-            print(f"Ошибка при запросе к странице. Код ответа: {response.status_code}")
-
-    except requests.RequestException as e:
-        print(f"Ошибка при запросе к сайту: {e}")
-
-def display_retry_link():
-    # Функция для отображения ссылки для повторного запроса
-    html = """
-    <a href="javascript:void(0)" onclick="window.location.reload();">Повторить запрос</a>
-    <p>При нажатии на ссылку страница будет обновлена, и можно будет ввести новый email.</p>
-    """
-    display(HTML(html))
-
-def main():
-    send_email()
-
-if __name__ == "__main__":
-    main()
+            print('На странице не найдено нужного текста. Проверьте доступность страницы.')
+    else:
+        print(f"Ошибка при запросе к странице. Код ответа: {response.status_code}")
+        
+except requests.RequestException as e:
+    print(f"Ошибка при запросе к сайту: {e}")
