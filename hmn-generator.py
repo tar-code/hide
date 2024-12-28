efc4a53157d70abb472d64ea69d1fec08598e395
TarCode <viktorprishchepa@yandex.ru>
2024-12-28 14:45:05 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 
 url = 'https://hdmn.cloud/ru/demo/'
 
@@ -9,20 +8,18 @@ try:
     
     # Проверка на успешный ответ от сервера
     if response.status_code == 200:
-        # Использование BeautifulSoup для парсинга HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Проверка наличия текста "Ваша электронная почта" на странице
-        if 'Ваша электронная почта' in soup.get_text():
+        # Если текст на странице содержит "Ваша электронная почта", продолжаем
+        if 'Ваша электронная почта' in response.text:
             email = input('Введите электронную почту для получения тестового периода: ')
 
-            # Отправка данных на сервер
-            response = requests.post('https://hdmn.cloud/ru/demo/success/', data={"demo_mail": email})
+            response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
+                "demo_mail": f"{email}"
+            })
 
-            if response.status_code == 200 and 'Ваш код выслан на почту' in response.text:
+            if 'Ваш код выслан на почту' in response.text:
                 print('Подтвердите e-mail. Проверьте свой почтовый ящик.')
             else:
-                print('Указанная почта не подходит для получения тестового периода или ошибка на сервере.')
+                print('Указанная почта не подходит для получения тестового периода.')
         else:
             print('На странице не найдено нужного текста. Проверьте доступность страницы.')
     else:
