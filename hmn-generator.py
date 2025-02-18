c849f3b4fef137dc4eb1aa48f5e40a80fdc13c93
TarCode <viktorprishchepa@yandex.ru>
2025-02-18 18:35:36 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,36 +1,29 @@
 import requests
 import re
-import time
-from IPython.display import display, Javascript
-
-def restart_colab():
-    """Функция для автоматической перезагрузки среды Google Colab"""
-    display(Javascript("location.reload()"))
-    time.sleep(2)  # Небольшая задержка перед завершением
-    exit()
 
 url = 'https://hdmn.cloud/ru/demo/'
 
 try:
+    # Отправляем GET-запрос
     response = requests.get(url)
-    response.encoding = 'utf-8'
+    response.encoding = 'utf-8'  # Указываем кодировку
 
+    # Проверяем успешный ответ сервера
     if response.status_code == 200:
         if 'Ваша электронная почта' in response.text:
             email = input('Введите электронную почту для получения тестового периода: ')
 
+            # Проверяем корректность введённого email
             if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
                 print("❌ \033[1;31mНекорректный e-mail. Пожалуйста, введите правильный адрес.\033[0m")
                 exit()
 
+            # Отправляем POST-запрос
             response = requests.post('https://hdmn.cloud/ru/demo/success/', data={"demo_mail": email})
 
+            # Проверяем успешность запроса
             if response.status_code == 200 and 'Ваш код выслан на почту' in response.text:
                 print('✅ \033[1;32mВаш код уже в пути!\033[0m Проверьте свой почтовый ящик.')
-
-                # Автоматическая перезагрузка Colab
-                restart_colab()
-
             else:
                 print('⚠️ \033[1;31mУказанная почта не подходит для получения тестового периода.\033[0m')
         else:
