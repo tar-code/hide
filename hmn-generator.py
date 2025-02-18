94c167cb30d13e71efc7514cec84542398aab626
TarCode <viktorprishchepa@yandex.ru>
2025-02-18 18:49:31 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -2,28 +2,29 @@ import requests
 
 url = 'https://hdmn.cloud/ru/demo/'
 
-# Попытка получить страницу и проверить статус ответа
 try:
     response = requests.get(url)
-    
-    # Проверка на успешный ответ от сервера
+    response.encoding = 'utf-8'  # Установка кодировки для корректного чтения текста
+
     if response.status_code == 200:
-        # Если текст на странице содержит "Ваша электронная почта", продолжаем
         if 'Ваша электронная почта' in response.text:
             email = input('Введите электронную почту для получения тестового периода: ')
+            
+            try:
+                response = requests.post('https://hdmn.cloud/ru/demo/success/', data={"demo_mail": email})
+                response.encoding = 'utf-8'
 
-            response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
-                "demo_mail": f"{email}"
-            })
+                if response.status_code == 200 and 'Ваш код выслан на почту' in response.text:
+                    print('✅ \033[1;32mВаш код уже в пути!\033[0m Проверьте свой почтовый ящик.')
+                else:
+                    print('⚠️ \033[1;31mУказанная почта не подходит для получения тестового периода.\033[0m')
 
-            if 'Ваш код выслан на почту' in response.text:
-                print('✅ \033[1;32mВаш код уже в пути!\033[0m Проверьте свой почтовый ящик.')
-            else:
-                print('⚠️ \033[1;31mУказанная почта не подходит для получения тестового периода.\033[0m')
+            except requests.RequestException as e:
+                print(f"❌ \033[1;31mОшибка при отправке почты:\033[0m {e}")
         else:
             print('⚠️ \033[1;31mНа странице не найдено нужного текста.\033[0m Проверьте доступность страницы.')
     else:
         print(f"❌ \033[1;31mОшибка при запросе к странице.\033[0m Код ответа: {response.status_code}")
-        
+
 except requests.RequestException as e:
     print(f"❌ \033[1;31mОшибка при запросе к сайту:\033[0m {e}")
